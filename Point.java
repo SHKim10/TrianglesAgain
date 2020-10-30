cf869e54b841c74fa1a39b2b2ccf1b72f5b2b36f
Stacy <skim10@stuy.edu>
2020-10-30 12:53:11 -0400

    Changed equals

--- Point.java
+++ Point.java
@@ -1,20 +1,31 @@
 public class Point{
   private double x,y;
+
+  //construct a point given coordinates
   public Point(double X, double Y){
     x = X;
     y = Y;
   }
+
+  //construct a point given a point (redundant, just provide flexibility)
   public Point(Point p){
-    x = p.x;
+    x = p.x;  //privaate access in the same file is allowed!
     y = p.y;
   }
+
   public double getX(){
     return x;
   }
+
   public double getY(){
     return y;
   }
+
   public double distanceTo(Point other){
     return Math.sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y));
+  }
+
+  public boolean equals(Point other){
+    return other != null && x == other.x && y == other.y;
   }
 }
